36d99796c5d6a3290bc161173cd982a5f73d7295
kyh174 <kyh174@naver.com>
2026-03-02 12:38:34 +0900

    chore: <header> 랜더러 제거

--- public/js/common/config.js
+++ public/js/common/config.js
@@ -4,26 +4,20 @@ var appConfig = new function() {
 
     var self = this;
 
-    // 헤더를 렌더링합니다.
-    // options.suffix  : 앱 이름 뒤에 붙는 페이지 제목 (예: 'World Builder')
-    // options.extra   : 헤더 안에 추가할 HTML 문자열 (예: input, language 선택 등)
-    this.renderHeader = function(options) {
-        options = options || {};
-        var suffix = options.suffix || '';
-        var extra = options.extra || '';
-        var displayName = suffix ? self.name + ' ' + suffix : self.name;
+    document.addEventListener('DOMContentLoaded', function() {
+        var logoEl = document.querySelector('header .gearsIcon');
+        if (logoEl) logoEl.src = self.logo;
 
-        var headerEl = document.querySelector('header');
-        if (!headerEl) return;
-
-        headerEl.innerHTML =
-            '<img class="gearsIcon" src="' + self.logo + '">' +
-            '<div class="appName">' + displayName + '</div>' +
-            extra;
+        var nameEl = document.querySelector('header .appName');
+        if (nameEl) {
+            var nameSuffix = nameEl.textContent.trim();
+            nameEl.textContent = nameSuffix ? self.name + ' ' + nameSuffix : self.name;
+        }
 
         var favicon = document.querySelector('link[rel="icon"]');
         if (favicon) favicon.href = self.logo;
 
-        document.title = displayName;
-    };
+        var pageSuffix = document.title.trim();
+        document.title = pageSuffix ? self.name + ' ' + pageSuffix : self.name;
+    });
 };

aa3020852490e4237d2155bdcd2304ebc743539f
kyh174 <kyh174@naver.com>
2026-03-03 11:34:04 +0900

    chore: scss 컴파일시 main.css 덮어 쓰지 않게 수정 - scss 사용 안함 - main.css 직접 수정할 것

--- sass_watch.sh
+++ sass_watch.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# ⚠️  주의: public/css/main.css 는 SCSS 빌드 대상에서 제외됩니다.
+# main.css 는 직접 편집하는 파일입니다. scss/main.scss 로 덮어쓰지 마세요.
+# widgets.css 만 SCSS 빌드 대상입니다.
+
 pushd scss
-sass --sourcemap=none --watch .:../public/css
+sass --sourcemap=none --watch widgets.scss:../public/css/widgets.css
 popd
